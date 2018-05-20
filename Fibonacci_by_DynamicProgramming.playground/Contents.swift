5fab9e9abb0bff59f521c7a79d6302c14b0b2911
ldindu <lakpadindu@hotmail.com>
2018-05-20 18:48:21 +0100

    Added enhanced version of fibnoccai implementation

--- Fibonacci_by_DynamicProgramming.playground/Contents.swift
+++ Fibonacci_by_DynamicProgramming.playground/Contents.swift
@@ -13,4 +13,21 @@ func fib_dp(n: Int) -> UInt {
   return f[n]
 }
 
-print(fib_dp(n: 94))
+// It appears we do not need to store all the intermediate value for the entire period of execution.
+// Because the recurrence depends on two arguments, we only need to retain the alst two values we have seen earlier.
+
+func fib_dp2(value: Int) -> UInt {
+  var back2: UInt = 0 // last two values of f[n]
+  var back1: UInt = 1
+  var next: UInt
+  
+  for _ in 2 ..< value {
+    next = UInt(back1 + back2)
+    back2 = back1
+    back1 = next
+  }
+  return back1 + back2
+}
+
+print(fib_dp(n: 93))
+print(fib_dp2(value: 93))
